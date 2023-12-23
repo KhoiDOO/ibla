297716813b8cc6ec03b2992015948819abbffc7f
KhoiDOO <khoido8899@gmail.com>
2023-12-23 14:03:15 +0700

    remove old clean loss function

--- loss/loss_fn/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dice_loss import dice_loss
-from .getloss import loss_dict, loss_batch
--- loss/lossmap.py
+++
@@ -1,8 +0,0 @@
-from .vanilla import Vanilla
-
-loss_mapping = {
-    "vanilla" : Vanilla
-}
-
-def loss_map(args):
-    return loss_mapping[args.loss](args=args)
