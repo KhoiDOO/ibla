f77150b371222eea76403a31b6be0cf4ed332c37
KhoiDOO <khoido8899@gmail.com>
2024-01-01 23:49:09 +0700

    update short import;

--- loss/__init__.py
+++ loss/__init__.py
@@ -5,3 +5,6 @@ from .vanilla_clf_stable import VanillaClassifierStableV0, VanillaClassifierStab
 from .vanilla_seg_stable import VanillaSegmenterStableV0, VanillaSegmenterStableV1
 
 from .focal import FocalClassifierV0, FocalSegmenterV0
+
+from .class_balance_loss import CBClassifierV0, CBSegmenterV0
+from .cbfocal import CBFocalClassifierV0, CBFocalSegmenterV0
