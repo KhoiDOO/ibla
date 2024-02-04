6735be7ca282556fbc03546cb33d85d7bd6725d7
KhoiDOO <khoido8899@gmail.com>
2024-02-04 13:42:34 +0700

    updateloss dwa

--- loss/hdlrw.py
+++ loss/hdlrw.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 from torch._tensor import Tensor 
 import torch.nn.functional as F
+import numpy as np
 
 from .vanilla_clf_stable import VanillaClassifierStableV0
 
@@ -93,3 +94,33 @@ class HDLRWClassifierV0(VanillaClassifierStableV0):
         else:
             self.epoch += 1
             return (-1 / B) * sum(list(sum_cls_loss.values()))
+    
+
+class HDLRWSegmenterV0(VanillaClassifierStableV0):
+    def __init__(self, args) -> None:
+        super().__init__(args)
+
+        self.epoch = 0
+        self.train_loss_buffer = np.zeros([self.args.seg_n_classes, self.args.epochs])
+
+    def forward(self, pred: Tensor, target: Tensor) -> Tensor:
+        logits = self.act(pred)
+
+        B, C, H, W = tuple(logits.size())
+
+        entropy = logits * target
+
+        class_entropy = torch.sum(entropy, axis = [0, 2, 3]).clamp(0.00001)
+
+        self.train_loss_buffer[:, self.epoch] = class_entropy.clone().tolist()
+
+        if self.epoch > 1:
+            w_i = torch.Tensor(self.train_loss_buffer[:, self.epoch-1] / self.train_loss_buffer[:, self.epoch-2]).to(self.device)
+            batch_weight = self.args.seg_n_classes * F.softmax(w_i/self.args.T, dim=-1)
+            weght_entropy = class_entropy * batch_weight
+            loss = (-1 / (B * H * W)) * torch.sum(class_entropy)
+        else:
+            loss = (-1 / (B * H * W)) * torch.sum(class_entropy)
+
+        self.epoch += 1
+        return loss
