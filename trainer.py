f83fc24ef4ad55cd3c30361c5d535876aa0bd889
KhoiDOO <khoido8899@gmail.com>
2024-02-07 23:11:15 +0700

    fix BUG wrong seed setting

--- trainer.py
+++ trainer.py
@@ -22,12 +22,8 @@ def train_func(args):
     # seed setup
     random.seed(args.seed)
     np.random.seed(args.seed)
-    torch.seed(args.seed)
     torch.manual_seed(args.seed)
-    torch.initial_seed(args.seed)
-    torch.cuda.seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-    torch.cuda.initial_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
     torch.backends.cudnn.deterministic = True
 
